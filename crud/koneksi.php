1e73a7645513f13c93d48a9d9d6ea3df6b447869
GraceTengko <tengkograce@gmail.com>
2024-11-25 10:23:05 +0800

    code baru

--- crud/koneksi.php
+++ crud/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 
-$host="localhost";
-$user="root";
-$password="";
+$host="103.23.198.122";
+$user="Devs7";
+$password="Team_Devops_7";
 $db="mahasiswa_db";
 
 $kon = mysqli_connect($host,$user,$password,$db);
